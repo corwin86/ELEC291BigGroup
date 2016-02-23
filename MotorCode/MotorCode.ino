5e981f9577311eaad118b5c51d79631d9047d679
Xingyu Tao <taoxingyu@gmail.com>
2016-02-23 08:45:48 -0800

    motor

--- MotorCode/MotorCode.ino
+++ MotorCode/MotorCode.ino
@@ -1,9 +1,14 @@
 void setup() {
-  // put your setup code here, to run once:
+  //set up channels A & B with respecetive pins
+  pinMode(12, OUTPUT);  //Motor A pin
+  pinMode(9, OUTPUT);   //Brake A pin
+
+  pinMode(13, OUTPUT);  //Motor B pin
+  pinMode(8, OUTPUT);   //Brake B pin
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // 
 
 }
