3276abc2ace4b064ffa946ff1f48b839990c4eef
Corwin Shanner <corwin.shanner@gmail.com>
2016-02-23 09:42:09 -0800

    sweep function works

--- tape_follow/tape_follow.ino
+++ tape_follow/tape_follow.ino
@@ -1,24 +1,35 @@
 #include <Servo.h>
 
-#define TRIGGER
-#define ECHO
-#define TEMPERATURE
+//define pins
+#define SERVO 10
+#define TRIGGER 13
+#define ECHO 11
+#define TEMPERATURE 3
+
+#define SLOW_DIST 30  //cm
+#define STOP_DIST 10  //cm
+#define STOP 0
+//#define MAX_SPEED     //TBD by motor people
+//#define SLOW_INC      //TBD by motor people
+#define LEFT -90
+#define RIGHT 90
 
 // ==== VARIABLES ====
-
-/**** SENSOR CONTROL ****/
-int rangePos = 0;    // variable to store the servo position for range finder
-
 Servo myservo;  // create servo object to control a servo
-/** END SENSOR CONTROL **/
 
+float temp;
+float pulse;
+float distance;
+float SpeedOfSound;
 // == END VARIABLES ==
 
 void setup() {
   // initialize serial monitor
   Serial.begin(9600);
-  // attaches the servo on pin 9 to the servo object
-  myservo.attach(9);
+  // attaches the servo on pin 10 to the servo object
+  myservo.attach(SERVO);
+  myservo.write(90);
+    
 
   // rangefinder pin modes
   pinMode(TRIGGER, OUTPUT);
@@ -28,28 +39,33 @@ void setup() {
 }
 
 void loop() {
-
-  if (ping() < 30) 
-    printf(sweep();
+  f1_loop();
 }
 
 /**
- * Rotates the server to the specified position
- * 
- * Param: pos - the position to rotate to. For reference, when the rangefinder is facing
- * the same direction as the robot, the position is 90. When the rangefinder is facing to the
- * left, the position is 0, and when it is facing to the right, the position is 180.
+ * Loop for function 1: roomba
  */
-void moveSensor(int pos) {
-  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
-  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
+void f1_loop(){
+    int dist = ping();
+    
+    if (dist > SLOW_DIST) 
+      moveForward(MAX_SPEED);
+      
+    else if (dist > STOP_DIST){
+      int myspeed = MAX_SPEED;
+      while (ping() > STOP_DIST){
+        moveForward(myspeed);
+        myspeed -= SLOW_INC;
+        delay(10);
+      }
+    }
+    else {
+      moveForward(STOP);
+      if (sweep() == LEFT)
+        turn(LEFT);
+      else
+        turn(RIGHT);
+    }
 }
 
 /**
@@ -59,16 +75,24 @@ void moveSensor(int pos) {
  * Returns: 1 if the longest distance read is on the left, 0 if the longest distance is on the right
  */
 int sweep() {
-  moveSensor(0);
+  myservo.write(0);
+  delay(1500);
   float leftDist = ping();
   
-  moveSensor(180);
+  myservo.write(180);
+  delay(1500);
   float rightDist = ping();
-  moveSensor(90);
+
+  Serial.print("\nDistance to the left: ");
+  Serial.print(leftDist);
+  Serial.print("\nDistance to the right: ");
+  Serial.print(rightDist);
+  
+  myservo.write(90);
   if (leftDist > rightDist)
-    return 1;
+    return LEFT;
   else
-    return 0;
+    return RIGHT;
 }
 
 /**
