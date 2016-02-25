a36a9390763b765a0ce2a5eed11a977ad719cd60
Xingyu Tao <taoxingyu@gmail.com>
2016-02-25 08:37:44 -0800

    stuff

--- MotorCode/MotorCode.ino
+++ MotorCode/MotorCode.ino
@@ -1,35 +1,35 @@
-#define MOTOR_A 12
-#define MOTOR_B 13
-#define BRAKE_A 9
-#define BRAKE_B 8
-#define SPEED_A 3
-#define SPEED_B 11
+#define MOTOR_A 5
+#define MOTOR_B 6
+#define BRAKE_A 
+#define BRAKE_B 
+#define SPEED_A 4
+#define SPEED_B 7
 
 
 
 void setup() {
   //set up channels A & B with respecetive pins
   pinMode(MOTOR_A, OUTPUT);  //Motor A pin
-  pinMode(BRAKE_A, OUTPUT);   //Brake A pin
+  //pinMode(BRAKE_A, OUTPUT);   //Brake A pin
 
   pinMode(MOTOR_B, OUTPUT);  //Motor B pin
-  pinMode(BRAKE_B, OUTPUT);   //Brake B pin
+  //pinMode(BRAKE_B, OUTPUT);   //Brake B pin
 
 }
 
 void loop() {
   digitalWrite(MOTOR_A, HIGH);
-  digitalWrite(BRAKE_A, LOW);
+  //digitalWrite(BRAKE_A, LOW);
   analogWrite(SPEED_A, 255);
 
   digitalWrite(MOTOR_B, HIGH);
-  digitalWrite(BRAKE_B, LOW);
+ // digitalWrite(BRAKE_B, LOW);
   analogWrite(SPEED_B, 255);
 
   delay(3000);
 
-  digitalWrite(BRAKE_A, HIGH);
-  digitalWrite(BRAKE_B, HIGH);
+ // digitalWrite(BRAKE_A, HIGH);
+ // digitalWrite(BRAKE_B, HIGH);
 
   delay(2000);
 
