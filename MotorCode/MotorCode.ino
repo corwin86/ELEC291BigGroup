41f2e6c2afc2dab8933879d7fdc53e27003a5200
Xingyu Tao <taoxingyu@gmail.com>
2016-02-26 14:44:53 -0800

    stuff

--- MotorCode/MotorCode.ino
+++ MotorCode/MotorCode.ino
@@ -8,49 +8,24 @@
 #define LEFT 1
 #define RIGHT 0
 
+#define 
+
 void setup() {
   //set up channels A & B with respecetive pins
   pinMode(MOTOR_A, OUTPUT);  //Motor A pin
-  //pinMode(BRAKE_A, OUTPUT);   //Brake A pin
-
   pinMode(MOTOR_B, OUTPUT);  //Motor B pin
-  //pinMode(BRAKE_B, OUTPUT);   //Brake B pin
+
 }
 
 void loop() {
 
   digitalWrite(MOTOR_A, HIGH);
-  //digitalWrite(BRAKE_A, LOW);
   analogWrite(SPEED_A, 255);
 
   digitalWrite(MOTOR_B, HIGH);
- // digitalWrite(BRAKE_B, LOW);
   analogWrite(SPEED_B, 255);
 
   delay(3000);
-
- // digitalWrite(BRAKE_A, HIGH);
- // digitalWrite(BRAKE_B, HIGH);
-
-  delay(2000);
-
-}
-
-  digitalWrite(MOTOR_A, HIGH);
-  //digitalWrite(BRAKE_A, LOW);
-  analogWrite(SPEED_A, 255);
-
-  digitalWrite(MOTOR_B, HIGH);
- // digitalWrite(BRAKE_B, LOW);
-  analogWrite(SPEED_B, 255);
-
-  delay(3000);
-
- // digitalWrite(BRAKE_A, HIGH);
- // digitalWrite(BRAKE_B, HIGH);
-
-  delay(2000);
-
 }
 
 void slowTurn(int direction){
